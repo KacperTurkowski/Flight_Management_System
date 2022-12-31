43c8bb03d5b3285f9a9bd33fca434945e2414b6a
kacperturkowski <kacperturkowski27@gmail.com>
2022-12-31 16:28:19 +0100

    add crew view

---
+++ FlightManagment/FlightManagment/Crew/CrewView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FlightManagement.Crew
+{
+    /// <summary>
+    /// Interaction logic for CrewView.xaml
+    /// </summary>
+    public partial class CrewView : UserControl
+    {
+        public CrewView()
+        {
+            InitializeComponent();
+        }
+    }
+}
